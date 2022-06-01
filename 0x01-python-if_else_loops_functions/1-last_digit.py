047e3e19256388a2a76039bd4c103870d3cfa25c
Samaybyte <samisole1996@gmail.com>
2022-06-01 13:29:25 -0700

    fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ comp = int(last_d)
 if number < 0:
     comp = -1 * comp
 if comp > 5:
-    print(f"Last digit of {number} is {last_d} and is greater than 5")
+    print(f"Last digit of {number} is {comp} and is greater than 5")
 elif comp == 0:
-    print(f"Last digit of {number} is {last_d} and is 0")
+    print(f"Last digit of {number} is {comp} and is 0")
 else:
-    print(f"Last digit of {number} is {last_d} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {comp} and is less than 6 and not 0")
