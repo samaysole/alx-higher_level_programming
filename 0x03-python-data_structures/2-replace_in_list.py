41751dd0e4570763caf772de71cf26aec7e4cdc6
Samaybyte <samisole1996@gmail.com>
2022-06-06 13:50:44 -0700

    updated

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
-        my_list.pop(idx)
-        my_list.insert(idx, element)
+    if (0 <= idx < len(my_list)):
+        my_list[idx] = element
     return my_list
-    else:
-        return my_list
