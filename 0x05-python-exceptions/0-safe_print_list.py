233fd23c748ae2f710bc5983b5496927cc54daf7
Samaybyte <samisole1996@gmail.com>
2022-06-21 07:07:22 -0700

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
         except:
             break
         else:
-            num +=1
+            num += 1
     print('')
-    return (num) 
+    return (num)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-
     try:
         response = a / b
     except ZeroDivisionError:
@@ -8,4 +7,4 @@ def safe_print_division(a, b):
     finally:
         print('Inside result: {}'.format(response))
 
-    return response 
+    return response
