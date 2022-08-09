355f3e5c20bc153ce0a4fe19505139aa9664c841
skahsay <skahsay@paga.com>
2022-08-09 22:42:02 +0300

    added

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- a script that creates a table called first_table in the current database
+-- and its description is 'id' (INT) and 'name' (VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table(id INT NOT NULL, name VARCHAR(256) NOT NULL);
