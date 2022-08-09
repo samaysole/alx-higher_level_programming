916dbd85e28db2b83a8e942ab794704e1589f24d
skahsay <skahsay@paga.com>
2022-08-10 00:10:20 +0300

    updated

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -4,7 +4,7 @@
 -- This throws an error if ONLY_FULL_GROUP_BY is enabled. This disable this,
 -- run: SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 SELECT city, month, AVG(value) AS avg_temp FROM temperatures
-WHERE (month=7 OR month=8)
+WHERE month=7 OR month=8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
