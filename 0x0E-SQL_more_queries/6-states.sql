7526716805627383c69a1db2ec75539f362af2fb
skahsay <skahsay@paga.com>
2022-08-10 23:20:08 +0300

    fix

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- states description: id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256) can’t be null
 -- if the table states already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL,
