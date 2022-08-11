92a89b42797a9e76ce661ff9918dc8831ee6df63
skahsay <skahsay@paga.com>
2022-08-11 12:42:36 +0300

    fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -13,5 +13,5 @@ COLLATE utf8mb4_unicode_ci;
 -- Convert field 'name' in 'first_table'
 USE hbtn_0c_0;
 ALTER TABLE first_table
-CHANGE name CONVERT TO CHARACTER SET utf8mb4
+CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
